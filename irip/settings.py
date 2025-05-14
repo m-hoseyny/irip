9c94dedfba20e2ea21475873d0c053a7da0e870c
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2025-05-14 16:29:00 -0400

    fix: change header of cors

--- irip/settings.py
+++ irip/settings.py
@@ -74,7 +74,17 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_HEADERS = default_headers + ('cache-control', 'Authentication', 'Authorization', 'authorization')
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 CORS_ALLOW_METHODS = [
     'DELETE',
     'GET',
