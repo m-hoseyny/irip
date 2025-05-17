ad7fb88bfbe7b3c26cff4200a3890da793aa74ca
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2025-05-17 14:09:17 -0400

    fix: setting remove whitenoise

--- irip/settings.py
+++ irip/settings.py
@@ -163,11 +163,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media/'
+print(MEDIA_ROOT)
 
-# Configure WhiteNoise to serve media files in production
-WHITENOISE_MEDIA_ROOT = MEDIA_ROOT
-WHITENOISE_MEDIA_URL = MEDIA_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
